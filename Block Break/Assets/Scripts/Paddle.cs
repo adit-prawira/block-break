f77df4b6262d4704367efcbfe9eaf5f8421057d4
Aditya Prawira <adityaprawira96@gmail.com>
2021-02-08 19:14:26 +1100

    Added scripts for draggable/interactive paddle

---
+++ Block Break/Assets/Scripts/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthUnits = 16f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePostUnits = (Input.mousePosition.x / Screen.width) * screenWidthUnits;
+        Vector2 paddlePost = new Vector2(mousePostUnits, transform.position.y);
+        transform.position = paddlePost;
+    }
+}
