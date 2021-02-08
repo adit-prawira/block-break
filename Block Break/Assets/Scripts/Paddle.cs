34639b6bf9dd6c40f41dfe702743e5a167d1f8ab
Aditya Prawira <adityaprawira96@gmail.com>
2021-02-08 19:19:11 +1100

    limit the paddle movement so that it won't go off screen

--- Block Break/Assets/Scripts/Paddle.cs
+++ Block Break/Assets/Scripts/Paddle.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    // Configuration Parameters
+    
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
     [SerializeField] float screenWidthUnits = 16f;
     // Start is called before the first frame update
     void Start()
@@ -12,10 +16,12 @@ public class Paddle : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
         float mousePostUnits = (Input.mousePosition.x / Screen.width) * screenWidthUnits;
-        Vector2 paddlePost = new Vector2(mousePostUnits, transform.position.y);
+        Vector2 paddlePost = new Vector2(transform.position.x, transform.position.y);
+        paddlePost.x = Mathf.Clamp(mousePostUnits, minX, maxX);
+
         transform.position = paddlePost;
     }
 }
