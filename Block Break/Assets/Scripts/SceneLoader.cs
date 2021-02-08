cf727119c9fc48f043317a7f4b009e9d59623100
Aditya Prawira <adityaprawira96@gmail.com>
2021-02-08 18:56:26 +1100

    Added collider to trigger game over scene

---
+++ Block Break/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene(){
+        // Find out the current index of the scene we currently on for this instance
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex+1);
+
+    }
+    public void LoadStartScene(){
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
